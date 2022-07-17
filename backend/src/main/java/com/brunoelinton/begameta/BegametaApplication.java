e910c921d8487eeed2d746afb446afb9bac80c4f
Bruno Guimarães <brunoelinton@outlook.com>
2022-07-16 21:46:31 -0300

    Project created

---
+++ backend/src/main/java/com/brunoelinton/begameta/BegametaApplication.java
@@ -0,0 +1,13 @@
+package com.brunoelinton.begameta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BegametaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BegametaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/brunoelinton/begameta/BegametaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunoelinton.begameta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BegametaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
