0cd8dbcbcfc87469456d5ab9f38613699f7add73
Bruno Guimarães <brunoelinton@outlook.com>
2022-07-21 16:29:58 -0300

    Frontend: NotificationId

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -9,8 +9,8 @@ type Props = {
 }
 
 function handleClick(id :number) {
-
-  axios(`${BASE_URL}/sales/${id}/notification`)
+  console.log(id);
+  axios(`${BASE_URL}/sales/10/notification`)
     .then(function () {
       toast.info("SMS enviado com sucesso");
     });
