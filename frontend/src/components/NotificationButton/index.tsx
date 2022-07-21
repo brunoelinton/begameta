d93446e7077b983afb1b8c0c27de2f545e55857e
Bruno Guimarães <brunoelinton@outlook.com>
2022-07-20 23:51:06 -0300

    Frontend: Configure Toastify

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import { toast } from 'react-toastify';
 import icon from '../../assets/img/notification-icon.svg';
 import { BASE_URL } from '../../utils/request';
 import './styles.css';
@@ -11,7 +10,7 @@ type Props = {
 function handleClick(id :number) {
   axios(`${BASE_URL}/sales/${id}/notification`)
     .then(response => {
-      toast.info("SMS enviado com sucesso");
+      // console.log("Sucesso");
     });
 }
 
